bcfe04ebd1a30b307dc61b0220d7fe57799eb284
Deception <32845547+Illusion0-0@users.noreply.github.com>
2021-09-07 21:41:33 +0530

    tweaks

--- src/team.js
+++ src/team.js
@@ -1,29 +1,33 @@
 import React from "react";
 import team_mem from './millennial.png';
+import github from './github.svg';
+import linkdin from './linkdin.svg';
+import instagram from './instagram.svg';
+import facebook from './facebook.svg';
 import "./App.css";
+//import "./circle.css";
 
 function team(props){
     return (
     <div className="container">
-    <div className="card">
-    <div className="imgBx">
-        <img className="circular--square" src={team_mem} alt="DSC"/>
+        <div className="card">
+            <div className="imgBx">
+                <img className="circular--square" src={team_mem} alt="DSC"/>
+            </div>
+            <div className="contentBx">
+                <h2>{props.name}</h2>
+                <div className="size">
+                    <h3>{props.role}</h3>
+                </div>
+                <div className="color">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+        </div>
     </div>
-    <div className="contentBx">
-    <h2>{props.name}</h2>
-    <div className="size">
-        <h3>{props.role}</h3>
-    </div>
-    <div className="color">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-    </div>
-    </div>
-    </div>
-    </div>
-
     );
 }
 
